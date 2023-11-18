3c398a9cd0ceb7d2e6ac6c39c2cb8bc6e75eac67
Jacob <jacatt04@gmail.com>
2023-11-18 13:36:18 +0100

    Task KU1.1 Done

---
+++ Domain/Models/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class Flight
+    {
+        public int Id { get; set; }
+        public string Rows { get; set; }
+        public string Columns { get; set; }
+        public string DepartureDate { get; set; }
+        public string ArrivalDate { get; set; }
+        public string CountryFrom { get; set; }
+        public string CountryTo { get; set; }
+        public string WholesalePrice { get; set; }
+        public string CommisionRate { get; set; }
+    }
+}
---
+++ Domain/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class Ticket
+    {
+        public int Id { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int FlightIdFK { get; set; }
+        public string Passport { get; set; }
+        public int PricePaid { get; set; }
+        public bool Cancelled { get; set; }
+    }
+}
